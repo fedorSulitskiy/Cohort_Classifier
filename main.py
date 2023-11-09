97651126e547683fe3a27cdac280d62acc4dc34b
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2023-11-09 16:09:49 +0000

    actual bruh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from smarts.smarts import CohortQualifier
+from news_search.news_search import harvey_helper
 app = FastAPI()
 
 CQ = CohortQualifier()
@@ -7,4 +8,9 @@ CQ = CohortQualifier()
 @app.get("/process/")
 async def process_input(input_str: str):
     result = CQ.classify(input_str)
+    return {"output": result}
+
+@app.get("/harvey/")
+async def process_input(input_str: str):
+    result = harvey_helper(input_str)
     return {"output": result}
