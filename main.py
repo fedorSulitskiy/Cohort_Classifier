a637466fb3bbe02081a3007d2f61fa2d068dd4e7
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2023-11-13 19:40:05 +0000

    small fix

--- main.py
+++ main.py
@@ -5,30 +5,33 @@ app = FastAPI()
 
 CQ = CohortQualifier()
 
+
 def description_path(desc: str):
     desc_words = desc.split()
-    
+
     if len(desc_words) < 50:
         return {
-            "result": "N/A",
+            "result": {"result": "N/A"},
             "source": "source",
             "total_cost": "",
         }
-    
+
     return CQ.scemantic_classify(desc, "desc")
-    
+
+
 @app.get("/process/")
 async def process_input(desc: str, website: str):
     if " " in website:
         inter = website.split()
         website = inter[0]
-        
+
     web_text = CQ.get_website_text(website)
-    
+
     if web_text[:5] == "Error" or len(web_text) < 500:
-        return description_path(desc)    
-    
+        return description_path(desc)
+
     return CQ.scemantic_classify(web_text[:2000], "web")
+
 
 @app.get("/length/")
 async def process_input(input_str: str):
