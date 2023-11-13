b780ec4c0f2d5d4a41ca760c37f17e5eeae6b6b8
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2023-11-13 18:34:13 +0000

    this was so fucking painful

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ app = FastAPI()
 
 CQ = CohortQualifier()
 
+def description_path(desc: str):
+    desc_words = desc.split()
+    
+    if len(desc_words) < 50:
+        return ""
+    
+    return CQ.scemantic_classify(desc, "desc")
+    
 @app.get("/process/")
-async def process_input(input_str: str):
-    result = CQ.classify(input_str)
-    return {"output": result}
+async def process_input(desc: str, website: str):
+    if " " in website:
+        inter = website.split()
+        website = inter[0]
+        
+    web_text = CQ.get_website_text(website)
+    
+    if web_text[:5] == "Error" or len(web_text) < 500:
+        return description_path(desc)    
+    
+    return CQ.scemantic_classify(web_text[:2000], "web")
 
 @app.get("/harvey/")
 async def process_input(input_str: str):
